e1a13a1fb304c20b8fcc6be223b3c3cf1f8a7418
yexuesong <yexuesong218@gmail.com>
2018-11-28 16:42:10 +0800

    add all category module and self-test pass

---
+++ src/main/java/com/onlineshoppingmall/service/ICategoryService.java
@@ -0,0 +1,16 @@
+package com.onlineshoppingmall.service;
+
+import com.onlineshoppingmall.common.ServerResponse;
+import com.onlineshoppingmall.pojo.Category;
+
+import java.util.List;
+
+public interface ICategoryService {
+    ServerResponse addCategory(String categoryName, Integer parentId);
+
+    ServerResponse updateCategoryName(Integer categoryId, String categoryName);
+
+    ServerResponse<List<Category>> getChildrenParallelCategory(Integer categoryId);
+
+    ServerResponse selectCategoryAndChildrenById(Integer categoryId);
+}
