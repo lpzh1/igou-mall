041087eae1ff6f20bc9956ab3a4bd563e24608f1
yexuesong <yexuesong218@gmail.com>
2018-12-06 21:28:04 +0800

    add cart module, but the response of product quantity is 0, need resolve!

---
+++ src/main/java/com/onlineshoppingmall/service/ICartService.java
@@ -0,0 +1,14 @@
+package com.onlineshoppingmall.service;
+
+import com.onlineshoppingmall.common.ServerResponse;
+import com.onlineshoppingmall.vo.CartVo;
+
+public interface ICartService {
+    ServerResponse<CartVo> add(Integer userId, Integer productId, Integer count);
+    ServerResponse<CartVo> update(Integer userId, Integer productId, Integer count);
+    ServerResponse<CartVo> deleteProduct(Integer userId, String productIds);
+
+    ServerResponse<CartVo> list(Integer userId);
+    ServerResponse<CartVo> selectOrUnSelect(Integer userId, Integer productId, Integer checked);
+    ServerResponse<Integer> getCartProductCount(Integer userId);
+}
