091b107e03c3e0ae5b4bed449f50cae9c171da06
yexuesong <yexuesong218@gmail.com>
2019-02-06 20:30:19 +0800

    commit cookie

--- src/main/java/com/onlineshoppingmall/util/CookieUtil.java
+++ src/main/java/com/onlineshoppingmall/util/CookieUtil.java
@@ -20,7 +20,7 @@ public class CookieUtil {
             for (Cookie ck : cks) {
                 log.info("read cookieName:{}, cookieValue:{}", ck.getName(), ck.getValue());
                 if (StringUtils.equals(ck.getName(), COOKIE_NAME)) {
-                    log.info("return cookieName:{}, cookieValue:{}", ck.getName(), ck.getValue());\
+                    log.info("return cookieName:{}, cookieValue:{}", ck.getName(), ck.getValue());
                     return ck.getValue();
                 }
             }
@@ -32,7 +32,7 @@ public class CookieUtil {
         Cookie ck = new Cookie(COOKIE_NAME, token);
         ck.setDomain(COOKIE_DOMAIN);
         ck.setPath("/"); // 代表设置在根目录
-
+        ck.setHttpOnly(true);
         //单位是秒
         //如果maxage不设置，cookie不会写入硬盘，而是写入内存，仅在当前页面有效
         ck.setMaxAge(60 * 60 * 24 * 365); // 如果为 -1，代表永久
