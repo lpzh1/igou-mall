b5b0f48e0d53a74c779dc4b8a56bc6450d298665
yexuesong <yexuesong218@gmail.com>
2019-02-10 21:59:33 +0800

    add spring session

--- src/main/java/com/onlineshoppingmall/pojo/User.java
+++ src/main/java/com/onlineshoppingmall/pojo/User.java
@@ -1,19 +1,20 @@
 package com.onlineshoppingmall.pojo;
 
-import com.google.common.collect.Lists;
+
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.io.Serializable;
 import java.util.Date;
-import java.util.List;
+
 
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-public class User {
+public class User implements Serializable {
     private Integer id;
 
     private String username;
