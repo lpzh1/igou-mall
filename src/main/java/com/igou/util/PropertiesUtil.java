aee8a983164a1d963c5841f3645b27400308ef2e
yexuesong <yexuesong@218@gmail.com>
2019-03-31 17:12:55 +0800

    修改项目名称及相关配置项

--- src/main/java/com/igou/util/PropertiesUtil.java
+++ src/main/java/com/igou/util/PropertiesUtil.java
@@ -13,7 +13,7 @@ public class PropertiesUtil {
     private static Properties props;
 
     static {
-        String fileName = "onlineshoppingmall.properties";
+        String fileName = "igou.properties";
         props = new Properties();
         try {
             props.load(new InputStreamReader(PropertiesUtil.class.getClassLoader().getResourceAsStream(fileName), "UTF-8"));
